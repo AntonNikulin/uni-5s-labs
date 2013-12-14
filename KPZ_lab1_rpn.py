08934288aacf87a2b5f40b1c1264ff1e8a9bc4a5
Anton Nikulin <dr.tallin@gmail.com>
2013-12-14 23:36:38 +0200

    -done- + comments

--- KPZ_lab1_rpn.py
+++ KPZ_lab1_rpn.py
@@ -22,19 +22,24 @@ def infixToPostfix(infix):
         elif el == '(':
             operatorsStack.push(el)
             
-        #from operatorsStack pop all operators until meeting left parenthesis
+        """from operatorsStack pop all operators until meet left parenthesis"""
         elif el == ')':
             topOp = operatorsStack.pop()
             while topOp != '(':
                 postfix.append(topOp)
                 topOp = operatorsStack.pop()
-
+                
+        """pop  all operators from stack that have
+        equal or higher precedence, append them to postfix, and
+        push the 'el' into the stack."""
         elif el in OPERATORS:
             while(not operatorsStack.isEmpty()) \
                       and (OPERATORS[operatorsStack.peek()] >= OPERATORS[el]):
                 postfix.append(operatorsStack.pop())
             operatorsStack.push(el)
-
+            
+    """remaining
+    operators from the stack to the postfix expression"""
     while not operatorsStack.isEmpty():
         postfix.append(operatorsStack.pop())
     return postfix
