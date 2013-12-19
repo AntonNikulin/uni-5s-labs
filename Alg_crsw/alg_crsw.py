d41ad6f392ebfa748f8f4c6ba4c744d22def1b15
Anton Nikulin <dr.tallin@gmail.com>
2013-12-19 20:23:18 +0200

    -done- Sum of right triangle in matrix

--- Alg_crsw/alg_crsw.py
+++ Alg_crsw/alg_crsw.py
@@ -8,15 +8,21 @@ matrix =[	[1, 1, 1, 1, 1, 1, 1, 1, 1, 2],
 			[1, 1, 1, 1, 1, 1, 2, 2, 2, 2],
 			[1, 1, 1, 1, 1, 1, 1, 2, 2, 2],
 			[1, 1, 1, 1, 1, 1, 1, 1, 2, 2],
-			[1, 1, 1, 1, 1, 1, 1, 1, 1, 2],]
+			[1, 1, 1, 1, 1, 1, 1, 1, 1, 2], ]
 
 
-length = len(matrix)
-sum = 0
-centerColumn = length/2
+columnLength = len(matrix)
+rowLength = len(matrix[0])
+sumMtr = 0
+centerColumn = columnLength/2
+rowStart = columnLength-1
+rowEnd = -1
+delta = 0
 
-for column in range(centerColumn,length):
-    for row in range(length-1,-1,-1):
-    	sum += matrix[row][column]
+for column in range(rowLength-1, centerColumn-1, -1):
+    for row in range(rowStart - delta , rowEnd + delta, -1):
+    	sumMtr += matrix[row][column]
     	print matrix[row][column]
-    	print "Sum is: {0}".format(sum)
+    	print "Sum is: {0}".format(sumMtr)
+
+    delta += 1
