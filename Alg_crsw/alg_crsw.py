e2f36ef9c36bb28bddcb0440c76f8bf0084771db
Anton Nikulin <dr.tallin@gmail.com>
2013-12-19 19:51:22 +0200

    Sum of the half

--- Alg_crsw/alg_crsw.py
+++ Alg_crsw/alg_crsw.py
@@ -10,11 +10,13 @@ matrix =[	[1, 1, 1, 1, 1, 1, 1, 1, 1, 2],
 			[1, 1, 1, 1, 1, 1, 1, 1, 2, 2],
 			[1, 1, 1, 1, 1, 1, 1, 1, 1, 2],]
 
+
 length = len(matrix)
 sum = 0
-column = length - 1
-minColumn = length/2
-for row in range(length-1,-1,-1):
-	sum += matrix[row][column]
-	print matrix[row][column]
-	print "Sum is: {0}".format(sum)
+centerColumn = length/2
+
+for column in range(centerColumn,length):
+    for row in range(length-1,-1,-1):
+    	sum += matrix[row][column]
+    	print matrix[row][column]
+    	print "Sum is: {0}".format(sum)
