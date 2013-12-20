b1625e1d0c8ba7148f7af539a6992ee525a0fbe9
Anton Nikulin <dr.tallin@gmail.com>
2013-12-20 12:04:03 +0200

    lab 2 + vertex and graph class

--- Alg_crsw/alg_crsw.py
+++ Alg_crsw/alg_crsw.py
@@ -1,6 +1,6 @@
 
 matrix =[
-    [1, 1, 1, 1, 1, 1, 1, 1, 100, 2],
+	[1, 1, 1, 1, 1, 1, 1, 1, 100, 2],
 	[1, 1, 1, 1, 1, 1, 1, 100, 2, 2],
 	[1, 1, 1, 1, 1, 1, 100, 2, 2, 2],
 	[1, 1, 1, 1, 1, 100, 2, 2, 2, 2],
@@ -17,18 +17,18 @@ rowLength = len(matrix[0])
 maxElem = 0
 maxElemRow = 0
 maxElemColumn = 0
-centerColumn = columnLength/2
+centerColumn = rowLength/2
 rowStart = columnLength-1
 rowEnd = -1
 delta = 0   #value to converge
 
 for column in range(rowLength-1, centerColumn-1, -1):
-    for row in range(rowStart - delta , rowEnd + delta, -1):
-    	if matrix[row][column] > maxElem:
-            maxElem = matrix[row][column]
-            maxElemRow = row
-            maxElemColumn = column
+	for row in range(rowStart - delta , rowEnd + delta, -1):
+		if matrix[row][column] > maxElem:
+			maxElem = matrix[row][column]
+			maxElemRow = row
+			maxElemColumn = column
 
-    delta += 1
+	delta += 1
 
 print "Max element is: {0} in position {1} : {2}".format(maxElem, maxElemRow, maxElemColumn)
